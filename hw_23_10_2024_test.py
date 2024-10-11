7572d80c5c3fc59c5def3420398f752836b7ec20
Dimon <d.serioussam@gmail.com>
2024-10-12 00:17:29 +0300

    add hw_23_10_2024

--- hw_23_10_2024_test.py
+++ hw_23_10_2024_test.py
@@ -2,23 +2,23 @@
 
 import unittest
 
-import hw_21_10_2024
+import hw_23_10_2024
 
 
 class TestIsPrime(unittest.TestCase):
     def test_arg_nan(self):
         """NaN test"""
-        result = hw_21_10_2024.is_prime('some string')
+        result = hw_23_10_2024.is_prime('some string')
         self.assertEqual(result, None)
 
     def test_arg_true(self):
         """True test"""
-        result = hw_21_10_2024.is_prime(17)
+        result = hw_23_10_2024.is_prime(17)
         self.assertTrue(result)
 
     def test_arg_false(self):
         """False test"""
-        result = hw_21_10_2024.is_prime(0)
+        result = hw_23_10_2024.is_prime(0)
         self.assertFalse(result)
 
 
