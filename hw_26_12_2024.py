74654c5eb37f8d0be227734fa19f5b85ec2118fb
Dimon <d.serioussam@gmail.com>
2024-12-19 00:46:20 +0300

    add 26(28).12.2024

--- hw_26_12_2024.py
+++ hw_26_12_2024.py
@@ -8,14 +8,29 @@ num_list = [-89, 0, -1, 2, 3, 4, -5]  # исходный список чисел
 def f(x):
     return x >= 0
 
-
-def filtr(func, number_list, arr=[]):
+# Вариант 1
+# def filtr(func, number_list, arr=[]):
+#     if len(number_list) == 0:  # базовый случай
+#         return arr
+#     else:  # рекурсивный случай
+#         if func(number_list[0]):
+#             arr.append(number_list[0])
+#         return filtr(func, number_list[1:], arr)
+    
+    
+# Вариант 2
+def filtr(func, number_list):
     if len(number_list) == 0:  # базовый случай
-        return arr
+        return []
     else:  # рекурсивный случай
         if func(number_list[0]):
-            arr.append(number_list[0])
-        return filtr(func, number_list[1:], arr)
+            return [number_list[0]] + filtr(func, number_list[1:])
+        else:
+            return filtr(func, number_list[1:])
+    
+    
+
+    
 
 
 print(filtr(f, num_list))
